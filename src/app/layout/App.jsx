6624c90d7f35c4de6dfa6cbc18469a16e315db10
tolkiana <tolkiana@gmail.com>
2018-09-02 19:59:16 -0400

    Add a raw nav bar component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div >
         <h1>Re-events</h1>
+        <NavBar/>
         <EventDashboard/>
       </div>
     );
