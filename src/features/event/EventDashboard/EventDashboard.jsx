d8f16242ba0de7f1b38b19af1bef0eea69d7891a
tolkiana <tolkiana@gmail.com>
2018-09-03 19:07:49 -0400

    Adding state to the Event Form

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -5,62 +5,76 @@ import EventForm from "../EventForm/EventForm";
 
 const eventsDashboard = [
   {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
-    category: 'culture',
+    id: "1",
+    title: "Trip to Tower of London",
+    date: "2018-03-27T11:00:00+00:00",
+    category: "culture",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    city: "London, UK",
     venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    hostedBy: "Bob",
+    hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
     attendees: [
       {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        id: "a",
+        name: "Bob",
+        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
       },
       {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        id: "b",
+        name: "Tom",
+        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
       }
     ]
   },
   {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
-    category: 'drinks',
+    id: "2",
+    title: "Trip to Punch and Judy Pub",
+    date: "2018-03-28T14:00:00+00:00",
+    category: "drinks",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    city: "London, UK",
+    venue: "Punch & Judy, Henrietta Street, London, UK",
+    hostedBy: "Tom",
+    hostPhotoURL: "https://randomuser.me/api/portraits/men/22.jpg",
     attendees: [
       {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        id: "b",
+        name: "Tom",
+        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
       },
       {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        id: "a",
+        name: "Bob",
+        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
       }
     ]
   }
-]
+];
 
 class EventDashboard extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       events: eventsDashboard,
       isOpen: false
-    }
+    };
+    this.handleFormOpen = this.handleFormOpen.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  handleFormOpen() {
+    this.setState({
+      isOpen: true
+    });
+  }
+
+  handleCancel() {
+    this.setState({
+      isOpen: false
+    });
   }
 
   render() {
@@ -70,8 +84,12 @@ class EventDashboard extends Component {
           <EventList events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button positive content="Create Event" />
-          {this.state.isOpen && <EventForm />}
+          <Button
+            onClick={this.handleFormOpen}
+            positive
+            content="Create Event"
+          />
+          {this.state.isOpen && <EventForm handleCancel={this.handleCancel} />}
         </Grid.Column>
       </Grid>
     );
