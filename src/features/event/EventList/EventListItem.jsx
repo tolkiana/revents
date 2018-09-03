2c4829925a75f2d3064c929acb392be4a756ac1d
tolkiana <tolkiana@gmail.com>
2018-09-02 21:18:49 -0400

    Format content

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
-import EventListAttendee from '../EventList/EventListAttendee'
+import EventListAttendee from "../EventList/EventListAttendee";
 
 class EventListItem extends Component {
   render() {
@@ -9,7 +9,11 @@ class EventListItem extends Component {
         <Segment>
           <Item.Group>
             <Item>
-              <Item.Image size="tiny" circular src="https://randomuser.me/api/portraits/women/42.jpg" />
+              <Item.Image
+                size="tiny"
+                circular
+                src="https://randomuser.me/api/portraits/women/42.jpg"
+              />
               <Item.Content>
                 <Item.Header as="a">Event Title</Item.Header>
                 <Item.Description>
@@ -26,13 +30,13 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-          <EventListAttendee/>
-          <EventListAttendee/>
-          <EventListAttendee/>
+            <EventListAttendee />
+            <EventListAttendee />
+            <EventListAttendee />
           </List>
         </Segment>
         <Segment clearing>
-        <span>Description will go here</span>
+          <span>Description will go here</span>
           <Button as="a" color="teal" floated="right" content="View" />
         </Segment>
       </Segment.Group>
