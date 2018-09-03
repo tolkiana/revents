320171e09418e0ccce0c8fdb0a42605d88a16519
tolkiana <tolkiana@gmail.com>
2018-09-02 20:13:07 -0400

    Put the dashboard inside a container and add a style for it

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
-import NavBar from '../../features/nav/NavBar/NavBar';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <div >
-        <h1>Re-events</h1>
-        <NavBar/>
-        <EventDashboard/>
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
