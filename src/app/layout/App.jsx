7d958208cccd8e091dafae07c7137bf19e1e5a5a
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-02 11:13:06 -0400

    Setup initial routing configuration

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,13 @@
-import React, { Component } from "react";
-import { Container } from "semantic-ui-react";
-import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import EventDetailPage from '../../features/event/EventDetail/EventDetailPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetail from '../../features/user/UserDetail/UserDetailPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
+import HomePage from '../../features/home/HomePage'
 import NavBar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
@@ -9,7 +16,13 @@ class App extends Component {
       <div>
         <NavBar />
         <Container className="main">
-          <EventDashboard />
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/events' component={EventDashboard}/>
+          <Route path='/events/:id' component={EventDetailPage}/>
+          <Route path='/people' component={PeopleDashboard}/>
+          <Route path='/profile/:id' component={UserDetail}/>
+          <Route path='/settings' component={SettingsDashboard}/>
+          <Route path='/createEvent' component={EventForm}/>
         </Container>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/layout/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./app/layout/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "semantic-ui-css/semantic.min.css";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(<App />, rootElement)
-}
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-    })
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
