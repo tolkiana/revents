9f71093f5760a9993e9f085410c7ef80f9306e6b
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-03 19:51:23 -0400

    Populate the event page with mock info

--- src/features/event/EventDetail/EventDetailPage.jsx
+++ src/features/event/EventDetail/EventDetailPage.jsx
@@ -5,16 +5,41 @@ import EventDetailInfo from "./EventDetailInfo";
 import EventDetailChat from "./EventDetailChat";
 import EventDetailSidebar from "./EventDetailSidebar";
 
+const event = {
+  id: "1",
+  title: "Trip to Tower of London",
+  date: "2018-03-27",
+  category: "culture",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+  city: "London, UK",
+  venue: "Tower of London, St Katharine's & Wapping, London",
+  hostedBy: "Bob",
+  hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
+  attendees: [
+    {
+      id: "a",
+      name: "Bob",
+      photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+    },
+    {
+      id: "b",
+      name: "Tom",
+      photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+    }
+  ]
+}
+
 const EventDetailPage = () => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventDetailHeader />
-        <EventDetailInfo />
+        <EventDetailHeader event={event} />
+        <EventDetailInfo event={event} />
         <EventDetailChat />
       </Grid.Column>
       <Grid.Column width={6}>
-        <EventDetailSidebar />
+        <EventDetailSidebar attendees={event.attendees} />
       </Grid.Column>
     </Grid>
   );
