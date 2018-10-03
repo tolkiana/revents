7b86bce95a8dc88df431d511df7afd1c7d723c9d
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-03 13:23:59 -0400

    Add redux deve tools and hot module replacement for the reducers

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,15 +1,25 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from '../reducers/rootReducer';
 
 export const configureStore = (preloadedState) => {
     const middlewares = [];
     const midlewareEnhancer = applyMiddleware(...middlewares);
     const storeEnhancers = [midlewareEnhancer];
-    const composedEnhancer = compose(...storeEnhancers)
+    const composedEnhancer = composeWithDevTools(...storeEnhancers)
     const store = createStore(
         rootReducer,
         preloadedState,
         composedEnhancer
     );
+
+    if (process.env.NODE_ENV !== 'production') {
+        if (module.hot) {
+            module.hot.accept('../reducers/rootReducer', () => {
+                const newRootReducer = require('../reducers/rootReducer').default;
+                store.replaceReducer(newRootReducer)
+            })
+        }
+    }
     return store;
 }
