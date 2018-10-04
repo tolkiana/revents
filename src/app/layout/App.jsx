2bd1088be6ef7f16ccbf81b50bd605941f4e4fde
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-04 11:13:53 -0400

    Add a path to route the event form

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -29,6 +29,7 @@ class App extends Component {
                   <Route path="/events" component={EventDashboard} />
                   <Route path="/test" component={TestComponent} />
                   <Route path="/event/:id" component={EventDetailPage} />
+                  <Route path="/manage/:id" component={EventForm} />
                   <Route path="/people" component={PeopleDashboard} />
                   <Route path="/profile/:id" component={UserDetail} />
                   <Route path="/settings" component={SettingsDashboard} />
