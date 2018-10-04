cfb84c5ca6133ea9a25fcfad3ccb89b687878acf
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-03 20:03:56 -0400

    Read the event from the store using the match params giving by the router

--- src/features/event/EventDetail/EventDetailPage.jsx
+++ src/features/event/EventDetail/EventDetailPage.jsx
@@ -1,36 +1,24 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
+import { connect } from 'react-redux'
 import EventDetailHeader from "./EventDetailHeader";
 import EventDetailInfo from "./EventDetailInfo";
 import EventDetailChat from "./EventDetailChat";
 import EventDetailSidebar from "./EventDetailSidebar";
 
-const event = {
-  id: "1",
-  title: "Trip to Tower of London",
-  date: "2018-03-27",
-  category: "culture",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
-  city: "London, UK",
-  venue: "Tower of London, St Katharine's & Wapping, London",
-  hostedBy: "Bob",
-  hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
-  attendees: [
-    {
-      id: "a",
-      name: "Bob",
-      photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
-    },
-    {
-      id: "b",
-      name: "Tom",
-      photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
-    }
-  ]
+const mapState = (state, ownProps) => {
+  const eventId = ownProps.match.params.id;
+  let event = {};
+  
+  if (eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0];
+  }
+  return {
+    event
+  }
 }
 
-const EventDetailPage = () => {
+const EventDetailPage = ({event}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
@@ -45,4 +33,4 @@ const EventDetailPage = () => {
   );
 };
 
-export default EventDetailPage;
+export default connect(mapState)(EventDetailPage);
