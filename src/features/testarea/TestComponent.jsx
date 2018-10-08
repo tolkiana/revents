a3b389002ef12625f3599994eac92c45af43e45a
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-08 10:03:55 -0400

    Test places autocomplete api

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,29 +1,70 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
-import { incrementCounter, decrementCounter } from './testActions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button } from "semantic-ui-react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
+import { incrementCounter, decrementCounter } from "./testActions";
+import Script from "react-load-script";
 
-const mapState = (state) => ({
-    data: state.test.data
-})
+const mapState = state => ({
+  data: state.test.data
+});
 
 const actions = {
-    incrementCounter,
-    decrementCounter
-}
+  incrementCounter,
+  decrementCounter
+};
 
 class TestComponent extends Component {
+  state = {
+    address: "",
+    scriptLoaded: false
+  };
+
+  handleScriptLoad = () => {
+    this.setState({scriptLoaded: true})
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
+
+  onChange = (address) => this.setState({ address });
+
   render() {
-    const {incrementCounter, decrementCounter, data} = this.props
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange
+    };
+    const { incrementCounter, decrementCounter, data } = this.props;
     return (
       <div>
+        <Script
+          url='https://maps.googleapis.com/maps/api/js?key=AIzaSyD3wyIgp5nP-maXpqJkRv9OJJNlMwCYsNA&libraries=places'
+          onLoad={this.handleScriptLoad}
+        />
         <h1>Test Component</h1>
         <h3>The answer is: {data}</h3>
-        <Button onClick={incrementCounter} color='green' content='Increment' />
-        <Button onClick={decrementCounter} color='red' content='Decrement' />
+        <Button onClick={incrementCounter} color="green" content="Increment" />
+        <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <br />
+        <form onSubmit={this.handleFormSubmit}>
+          {this.state.scriptLoaded && <PlacesAutocomplete inputProps={inputProps} />}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
 }
 
-export default connect(mapState, actions)(TestComponent);
+export default connect(
+  mapState,
+  actions
+)(TestComponent);
