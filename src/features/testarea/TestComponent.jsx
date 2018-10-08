b1892ab31b6faa536473e21f89b36f2d460d4bf4
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-08 16:35:59 -0400

    Adding the map to the test component

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
 } from "react-places-autocomplete";
 import { incrementCounter, decrementCounter } from "./testActions";
+import GoogleMapReact from "google-map-react";
 import Script from "react-load-script";
 
 const mapState = state => ({
@@ -17,15 +18,25 @@ const actions = {
   decrementCounter
 };
 
+const Marker = () => <Icon name="marker" size="big" color="red" />;
+
 class TestComponent extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   state = {
     address: "",
     scriptLoaded: false
   };
 
   handleScriptLoad = () => {
-    this.setState({scriptLoaded: true})
-  }
+    this.setState({ scriptLoaded: true });
+  };
 
   handleFormSubmit = event => {
     event.preventDefault();
@@ -36,7 +47,7 @@ class TestComponent extends Component {
       .catch(error => console.error("Error", error));
   };
 
-  onChange = (address) => this.setState({ address });
+  onChange = address => this.setState({ address });
 
   render() {
     const inputProps = {
@@ -47,7 +58,7 @@ class TestComponent extends Component {
     return (
       <div>
         <Script
-          url='https://maps.googleapis.com/maps/api/js?key=AIzaSyD3wyIgp5nP-maXpqJkRv9OJJNlMwCYsNA&libraries=places'
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3wyIgp5nP-maXpqJkRv9OJJNlMwCYsNA&libraries=places"
           onLoad={this.handleScriptLoad}
         />
         <h1>Test Component</h1>
@@ -56,9 +67,22 @@ class TestComponent extends Component {
         <Button onClick={decrementCounter} color="red" content="Decrement" />
         <br />
         <form onSubmit={this.handleFormSubmit}>
-          {this.state.scriptLoaded && <PlacesAutocomplete inputProps={inputProps} />}
+          {this.state.scriptLoaded && (
+            <PlacesAutocomplete inputProps={inputProps} />
+          )}
           <button type="submit">Submit</button>
         </form>
+        <div style={{ height: "300px", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyD3wyIgp5nP-maXpqJkRv9OJJNlMwCYsNA"
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <Marker lat={59.955413} lng={30.337844} text={"Kreyser Avrora"} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
