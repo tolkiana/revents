bf8e81f621e22930eccc4da712f4205f291076f5
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-15 10:45:35 -0400

    Implement async increment and decrement functions

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -5,17 +5,18 @@ import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
 } from "react-places-autocomplete";
-import { incrementCounter, decrementCounter } from "./testActions";
+import { incrementAsync, decrementAsync } from "./testActions";
 import { openModal } from '../modals/modalActions'
 import Script from "react-load-script";
 
 const mapState = state => ({
-  data: state.test.data
+  data: state.test.data,
+  loading: state.test.loading
 });
 
 const actions = {
-  incrementCounter,
-  decrementCounter,
+  incrementAsync,
+  decrementAsync,
   openModal
 };
 
@@ -53,7 +54,7 @@ class TestComponent extends Component {
       value: this.state.address,
       onChange: this.onChange
     };
-    const { incrementCounter, decrementCounter, data, openModal } = this.props;
+    const { incrementAsync, decrementAsync, data, openModal, loading } = this.props;
     return (
       <div>
         <Script
@@ -62,8 +63,8 @@ class TestComponent extends Component {
         />
         <h1>Test Component</h1>
         <h3>The answer is: {data}</h3>
-        <Button onClick={incrementCounter} color="green" content="Increment" />
-        <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <Button loading={loading} onClick={incrementAsync} color="green" content="Increment" />
+        <Button loading={loading} onClick={decrementAsync} color="red" content="Decrement" />
         <Button onClick={() => openModal('TestModal', {data: 43})} color="teal" content="Open Modal" />
         <br />
         <form onSubmit={this.handleFormSubmit}>
--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,17 +1,30 @@
-import { createReducer } from '../../app/common/util/reducerUtil'
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
+import { createReducer } from "../../app/common/util/reducerUtil";
+import {
+  INCREMENT_COUNTER,
+  DECREMENT_COUNTER,
+  COUNTER_ACTION_STARTED,
+  COUNTER_ACTION_FINISHED
+} from "./testConstants";
 
 const initialState = {
   data: 47
 };
 
 export const incrementCounter = (state, payload) => {
-    return { ...state, data: state.data + 1 };
-}
+  return { ...state, data: state.data + 1 };
+};
 
 export const decrementCounter = (state, payload) => {
-    return { ...state, data: state.data - 1 };
-}
+  return { ...state, data: state.data - 1 };
+};
+
+export const counterActionStarted = (state, payload) => {
+  return { ...state, loading: true };
+};
+
+export const counterActionsFinished = (state, payload) => {
+  return { ...state, loading: false };
+};
 
 // const testReducer = (state = initialState, action) => {
 //   switch (action.type) {
@@ -25,6 +38,8 @@ export const decrementCounter = (state, payload) => {
 // };
 
 export default createReducer(initialState, {
-    [INCREMENT_COUNTER]: incrementCounter,
-    [DECREMENT_COUNTER]: decrementCounter
+  [INCREMENT_COUNTER]: incrementCounter,
+  [DECREMENT_COUNTER]: decrementCounter,
+  [COUNTER_ACTION_STARTED]: counterActionStarted,
+  [COUNTER_ACTION_FINISHED]: counterActionsFinished
 });
