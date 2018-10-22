c7e9e5a8222cf3fd1483d97f96cc425b8256c4a2
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2018-10-21 21:07:51 -0400

    Adding a loading component

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { connect } from 'react-redux'
 import { Grid } from "semantic-ui-react";
 import EventList from "../EventList/EventList";
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 import { deleteEvent } from '../eventActions'
 
 const mapState = (state) => ({
-  events: state.events
+  events: state.events,
+  loading: state.async.loading
 })
 
 const actions = {
@@ -19,7 +21,8 @@ class EventDashboard extends Component {
   };
 
   render() {
-    const { events } = this.props
+    const { events, loading } = this.props;
+    if (loading) return <LoadingComponent/>
     return (
       <Grid>
         <Grid.Column width={10}>
